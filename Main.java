4592fc60f469d4fe925d6b6cd9f702b5c5f45e32
komkawila <kompanuwat@gmail.com>
2019-07-02 21:33:12 +0700

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String args[])
+	{
+//		int a=10;
+//		double price = 10.5;
+//		String name = "Phanuwat Kawila";
+//		
+//		int[] num = {1,2,3,4,5,9,6,8,9};
+//		for(int i=0;i<num.length;i++)
+//			System.out.println(num[i]);
+//		//System.out.println("Price = " + price);
+//		//System.out.println("Hello : " + name.toUpperCase());
+//		
+//		String[] fruits = {"banana","apple"};
+//		
+//		int[] num2 = new int[100];
+//		
+//		for(String f:fruits)
+//			System.out.println(f);
+		
+//		String tel ="053-211-222";
+//		String[] ts = tel.split("-");
+//		System.out.println(ts[0]);
+		Scanner Sc = new Scanner(System.in);
+		String s = Sc.nextLine();
+		System.out.println(s);
+	}
+}
